1e7b6c4941113bc9d76811d7cea84e8d260c728e
pativilaka <p.vilaka@gmail.com>
2023-10-24 15:13:38 -0300

    add ORM user and role

--- src/main/java/com/devsuperior/demo/entities/User.java
+++ src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,14 +1,11 @@
 package com.devsuperior.demo.entities;
 
-import java.time.LocalDate;
-import java.util.Objects;
+import jakarta.persistence.*;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
@@ -18,6 +15,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     @Column(unique = true)
     private String email;
@@ -63,6 +66,19 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void addRole(Role role){
+        addRole(role);
+    }
+
+    public boolean hasRole(String roleName){
+        for (Role role : roles) {
+            if(role.equals(roleName)){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
